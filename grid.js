35e0381dc8696bba8f25b21c8e33fd2d1fd5397a
lester2027 <lesterviloria1527@gmail.com>
2026-04-14 18:14:48 +0700

    feat: implement sidebar toggle

--- grid.js
+++ grid.js
@@ -1,3 +1,4 @@
+// Submenu for the menu of sidebar //
 function toggleMenu(element) {
   const submenu = element.nextElementSibling;
   const arrow = element.querySelector(".arrow");
@@ -5,3 +6,8 @@ function toggleMenu(element) {
   submenu.classList.toggle("show");
   arrow.classList.toggle("rotate");
 }
+
+// sidebar active //
+$(".menu-btn").click(function () {
+  $(".sidebar").toggleClass("active");
+});
